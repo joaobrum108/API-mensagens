6dd4b1bc80de76886fdfc4c13d419f8249bf55d5
joaobrum108 <joaobrum108gmail.com>
2025-07-24 15:27:36 -0300

    atualizado

--- src/services/serviceMensagem.js
+++ src/services/serviceMensagem.js
@@ -30,6 +30,29 @@ class serviceMensagem {
       });
     }
   }
+  // async enviarMensagemWhatsapp(req, res) {
+  //   const { mensagem } = req.body;
+  //   const ZAPI_INSTANCE = "3E4AC80F98D6D0B9180D42A445172BEF";
+  //   const ZAPI_TOKEN = "B23C505B816B90FF6FD9F00F";
+  //   const NUMERO_DESTINO = "5511968713163";
+  //   try {
+  //     const response = await axios.post(
+  //       `https://api.z-api.io/instances/${ZAPI_INSTANCE}/token/${ZAPI_TOKEN}/send-text`,
+  //       {
+  //         phone: NUMERO_DESTINO,
+  //         message: mensagem,
+  //       }
+  //     );
+  //     res.status(200).json({
+  //       message: "Mensagem enviada com sucesso",
+  //       data: response.data,
+  //     });
+  //   } catch (error) {
+  //     res.status(400).json({
+  //       message: "Erro ao enviar a mensagem",
+  //       error: error.message,
+  //     });
+  //   }
+  // }
 }
-
 module.exports = new serviceMensagem();
